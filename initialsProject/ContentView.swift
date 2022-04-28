444756cbf6842e596cfadb05d5a79c343a7ee94e
Juliano C L Santos <foxports@hotmail.com>
2022-04-28 00:06:34 -0300

    Initial Commit

---
+++ initialsProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  initialsProject
+//
+//  Created by Juliano Santos on 28/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ initialsProject/initialsProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  initialsProjectApp.swift
+//  initialsProject
+//
+//  Created by Juliano Santos on 28/4/22.
+//
+
+import SwiftUI
+
+@main
+struct initialsProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
